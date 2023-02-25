37580ecdb975f28c01576431fffe020ce9ccaabd
BorisKartofel <botisov-anton-0@mail.ru>
2023-02-25 20:03:02 +0300

    1. Created methods for client-server messages handling

--- src/java/app/Game.java
+++ src/java/app/Game.java
@@ -1,15 +1,21 @@
 package java.app;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Game {
-    /*
+/*
     class Main - класс на котором мы будем запускать приложение
     class Desk - описана логика прорисовки доски
     class Ship - описана логика взаимодействия с кораблем: узнать состояние, изменить состояние
     class Game - описана логика самой игры, правила
     class Multiplayer - описана логика подключения двух игроков к игре.
     class DBConnection - описано подключение к БД
-
-     */
+*/
     private class Desk {
 
     }
@@ -18,7 +24,32 @@ public class Game {
 
     }
 
-    private class Multiplayer {
+    private class Multiplayer{
+        public void clientSocket() throws IOException {
+            Socket socket = new Socket("localhost", 7777);
+
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+            Scanner scan = new Scanner(System.in);
+
+            String str = scan.nextLine();
+            writer.println(str);
+            scan.close();
+            writer.flush();
+        }
+
+        public void serverSocket() throws IOException {
+            ServerSocket serverSocket = new ServerSocket( 7777);
+            Socket socket = serverSocket.accept();
+
+            System.out.println("Client successfully connected by port "+ socket.getPort());
+
+            InputStreamReader reader = new InputStreamReader(socket.getInputStream());
+            Scanner scan = new Scanner(System.in);
+
+            String str = scan.nextLine();
+            System.out.println("Client wrote " + str);
+            scan.close();
+        }
 
     }
 
--- src/java/app/Main.java
+++ src/java/app/Main.java
@@ -1,8 +1,9 @@
 package java.app;
 
-public class Main {
 
+
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
