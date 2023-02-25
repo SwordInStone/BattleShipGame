e39ae3f8a569c8e2d80e422215c86c4d867db84d
Petr Sha <MilkCup@yandex.ru>
2023-02-25 19:27:56 +0500

    Добавил все файлы классов

---
+++ src/java/app/Game.java
@@ -0,0 +1,31 @@
+package java.app;
+
+public class Game {
+    /*
+    class Main - класс на котором мы будем запускать приложение
+    class Desk - описана логика прорисовки доски
+    class Ship - описана логика взаимодействия с кораблем: узнать состояние, изменить состояние
+    class Game - описана логика самой игры, правила
+    class Multiplayer - описана логика подключения двух игроков к игре.
+    class DBConnection - описано подключение к БД
+
+     */
+    private class Desk {
+
+    }
+
+    private class Ship {
+
+    }
+
+    private class Multiplayer {
+
+    }
+
+    private class DBConnection {
+
+    }
+
+}
+
+
